04f6d714f3607064315bb2695eea8a417e6e1ac6
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 16:49:08 +0300

    Finish answer question test

--- backend/tests/Feature/Interview/AnswerQuestionTest.php
+++ backend/tests/Feature/Interview/AnswerQuestionTest.php
@@ -2,19 +2,38 @@
 
 namespace Tests\Feature\Interview;
 
+use App\Models\InterviewQuestion;
+use App\Models\InterviewSession;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AnswerQuestionTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_answer_a_question()
+    {
+        //create user and question
+        $user = User::factory()->create();
+        $question = InterviewQuestion::factory()->create([
+            'session_id' => InterviewSession::factory()->create(['user_id' => $user->id])->id,
+        ]);
+
+        //answer question
+        $response = $this->actingAs($user)->patchJson("/api/questions/{$question->id}", [
+            'user_answer' => 'I am passionate about software engineering.',
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'user_answer' => 'I am passionate about software engineering.',
+            ]);
+
+        $this->assertDatabaseHas('interview_questions', [
+            'id' => $question->id,
+            'user_answer' => 'I am passionate about software engineering.',
+        ]);
     }
 }
