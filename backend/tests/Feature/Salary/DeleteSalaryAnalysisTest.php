444bcaa6465c4b041a420d8b9dc929427497293a
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 17:16:27 +0300

    Finish delete salary analysis test

--- backend/tests/Feature/Salary/DeleteSalaryAnalysisTest.php
+++ backend/tests/Feature/Salary/DeleteSalaryAnalysisTest.php
@@ -2,19 +2,29 @@
 
 namespace Tests\Feature\Salary;
 
+use App\Models\SalaryAnalysis;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DeleteSalaryAnalysisTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_delete_salary_analysis()
+    {
+        //create user and analysis
+        $user = User::factory()->create();
+        $salaryAnalysis = SalaryAnalysis::factory()->create(['user_id' => $user->id]);
+
+        //delete analysis
+        $response = $this->actingAs($user)->deleteJson("/api/salaries/{$salaryAnalysis->id}");
+
+        $response->assertStatus(204);
+
+        $this->assertDatabaseMissing('salary_analysis', [
+            'id' => $salaryAnalysis->id,
+        ]);
     }
 }
