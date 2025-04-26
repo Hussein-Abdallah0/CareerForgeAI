8e5e8a5b5c064c580dd6f6158d5273f39d6370a0
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 17:14:33 +0300

    Finish list salary analysis test

--- backend/tests/Feature/Salary/ListSalaryAnalysisTest.php
+++ backend/tests/Feature/Salary/ListSalaryAnalysisTest.php
@@ -2,19 +2,26 @@
 
 namespace Tests\Feature\Salary;
 
+use App\Models\SalaryAnalysis;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ListSalaryAnalysisTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_list_their_salary_analyses()
+    {
+        //create user and analysis
+        $user = User::factory()->create();
+        SalaryAnalysis::factory()->count(2)->create(['user_id' => $user->id]);
+
+        //get all analysis
+        $response = $this->actingAs($user)->getJson('/api/salaries');
+
+        $response->assertStatus(200)
+            ->assertJsonCount(2);
     }
 }
