6ed07854dd8266977e2ec0a991698258f52230ff
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 16:55:32 +0300

    Finish view session details test

--- backend/tests/Feature/Interview/ViewSessionDetailsTest.php
+++ backend/tests/Feature/Interview/ViewSessionDetailsTest.php
@@ -2,19 +2,37 @@
 
 namespace Tests\Feature\Interview;
 
+use App\Models\InterviewQuestion;
+use App\Models\InterviewSession;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ViewSessionDetailsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_view_interview_session_details()
+    {
+        //create user,session and questions
+        $user = User::factory()->create();
+        $session = InterviewSession::factory()->create(['user_id' => $user->id]);
+        InterviewQuestion::factory()->count(2)->create(['session_id' => $session->id]);
+
+        //get interview session details
+        $response = $this->actingAs($user)->getJson("/api/interviews/{$session->id}");
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'id',
+                'user_id',
+                'job_title',
+                'ai_feedback',
+                'created_at',
+                'questions' => [
+                    ['id', 'session_id', 'question', 'user_answer', 'ai_comment', 'created_at', 'updated_at'],
+                ]
+            ]);
     }
 }
