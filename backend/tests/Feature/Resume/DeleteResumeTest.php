1026edb6149f955c551fb7d60ffdd83ede2d4476
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 17:08:17 +0300

    Finish delete resume test

--- backend/tests/Feature/Resume/DeleteResumeTest.php
+++ backend/tests/Feature/Resume/DeleteResumeTest.php
@@ -2,19 +2,29 @@
 
 namespace Tests\Feature\Resume;
 
+use App\Models\Resume;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DeleteResumeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_delete_resume()
+    {
+        //create user and resume
+        $user = User::factory()->create();
+        $resume = Resume::factory()->create(['user_id' => $user->id]);
+
+        //delete resume
+        $response = $this->actingAs($user)->deleteJson("/api/resumes/{$resume->id}");
+
+        $response->assertStatus(204);
+
+        $this->assertDatabaseMissing('resumes', [
+            'id' => $resume->id,
+        ]);
     }
 }
