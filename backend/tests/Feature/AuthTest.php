c79d7385820e577c46aabeebbd084e76d3bd63bd
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 15:13:42 +0300

    Add feature test for testing if user can login

--- backend/tests/Feature/AuthTest.php
+++ backend/tests/Feature/AuthTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,6 +14,24 @@ class AuthTest extends TestCase
         $response = $this->postJson('/api/v1/register', [
             'first_name' => 'test',
             'last_name' => 'user',
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJsonStructure(['success', 'payload']);
+    }
+
+    public function test_user_can_login()
+    {
+        //create user with credentials
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        //test login with the above user credentials 
+        $response = $this->postJson('/api/v1/login', [
             'email' => 'test@example.com',
             'password' => 'password',
         ]);
