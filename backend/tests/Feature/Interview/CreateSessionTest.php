0378bb9d5aa0ce686fa482433fa8a80b0ca151f4
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 16:10:35 +0300

    Finish create interview session test

--- backend/tests/Feature/Interview/CreateSessionTest.php
+++ backend/tests/Feature/Interview/CreateSessionTest.php
@@ -2,19 +2,37 @@
 
 namespace Tests\Feature\Interview;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CreateSessionTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_create_interview_session()
+    {
+        //create user
+        $user = User::factory()->create();
+
+        //test if created user can create an interview session
+        $response = $this->actingAs($user)->postJson('/api/v1/interviews', [
+            'job_title' => 'Software Engineer',
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJsonStructure([
+                'id',
+                'user_id',
+                'job_title',
+                'ai_feedback',
+                'created_at',
+            ]);
+
+        $this->assertDatabaseHas('interview_sessions', [
+            'user_id' => $user->id,
+            'job_title' => 'Software Engineer',
+        ]);
     }
 }
