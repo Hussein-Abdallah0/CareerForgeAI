f185c4355e6385bfeab2c29f7333ec5a567c2bb2
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 17:15:23 +0300

    Finish view salary analysis test

--- backend/tests/Feature/Salary/ViewSalaryAnalysisTest.php
+++ backend/tests/Feature/Salary/ViewSalaryAnalysisTest.php
@@ -2,19 +2,34 @@
 
 namespace Tests\Feature\Salary;
 
+use App\Models\SalaryAnalysis;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ViewSalaryAnalysisTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_view_single_salary_analysis()
+    {
+
+        //create user and analysis
+        $user = User::factory()->create();
+        $salaryAnalysis = SalaryAnalysis::factory()->create(['user_id' => $user->id]);
+
+        //get analysis
+        $response = $this->actingAs($user)->getJson("/api/salaries/{$salaryAnalysis->id}");
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'id' => $salaryAnalysis->id,
+                'job_title' => $salaryAnalysis->job_title,
+                'location' => $salaryAnalysis->location,
+                'experience_level' => $salaryAnalysis->experience_level,
+                'current_salary' => $salaryAnalysis->current_salary,
+                'suggested_range' => $salaryAnalysis->suggested_range,
+            ]);
     }
 }
