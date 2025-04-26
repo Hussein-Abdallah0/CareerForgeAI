5aa9a0f3999ac5edece2251f5ad88cf9fea45aa8
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 16:14:15 +0300

    Add comments for AddQuestionTest

--- backend/tests/Feature/Interview/AddQuestionTest.php
+++ backend/tests/Feature/Interview/AddQuestionTest.php
@@ -14,9 +14,11 @@ class AddQuestionTest extends TestCase
 
     public function test_user_can_add_question_to_session()
     {
+        //create user and session for this user
         $user = User::factory()->create();
         $session = InterviewSession::factory()->create(['user_id' => $user->id]);
 
+        //add question
         $response = $this->actingAs($user)->postJson("/api/interviews/{$session->id}/questions", [
             'question' => 'Tell me about yourself.',
         ]);
