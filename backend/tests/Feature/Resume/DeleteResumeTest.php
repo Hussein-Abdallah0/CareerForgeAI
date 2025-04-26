f39d50adc83ae6f3c5a78ca2f842497de4e214db
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 17:04:30 +0300

    Add test for deleting resume

---
+++ backend/tests/Feature/Resume/DeleteResumeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Resume;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DeleteResumeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
