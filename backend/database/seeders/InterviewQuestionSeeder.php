962b903a61635452a0bb06a26fe32143cbc3bf28
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 12:55:42 +0300

    Finish interviewQuestionSeeder

--- backend/database/seeders/InterviewQuestionSeeder.php
+++ backend/database/seeders/InterviewQuestionSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\InterviewQuestion;
+use App\Models\InterviewSession;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,17 @@ class InterviewQuestionSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        // Make sure you have interview sessions first!
+        $sessions = InterviewSession::all();
+
+        foreach ($sessions as $session) {
+            InterviewQuestion::factory()
+                ->count(5) // 5 questions per session
+                ->create([
+                    'session_id' => $session->id,
+                ]);
+        }
     }
 }
