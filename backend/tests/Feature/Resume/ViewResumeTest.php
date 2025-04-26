f93728634f199755d8c93af8d61028cd8f2f9d0a
Hussein-02 <hussein02ab@gmail.com>
2025-04-26 17:07:30 +0300

    Finish view resume test

--- backend/tests/Feature/Resume/ViewResumeTest.php
+++ backend/tests/Feature/Resume/ViewResumeTest.php
@@ -2,19 +2,33 @@
 
 namespace Tests\Feature\Resume;
 
+use App\Models\Resume;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ViewResumeTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_can_view_single_resume()
+    {
+        // create user and resume
+        $user = User::factory()->create();
+        $resume = Resume::factory()->create(['user_id' => $user->id]);
+
+        //get resume
+        $response = $this->actingAs($user)->getJson("/api/resumes/{$resume->id}");
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'id' => $resume->id,
+                'job_title' => $resume->job_title,
+                'experience' => $resume->experience,
+                'skills' => $resume->skills,
+                'education' => $resume->education,
+                'tailored_resume' => $resume->tailored_resume,
+            ]);
     }
 }
