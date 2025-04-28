3e7dd3e0c6aa749f30ba7d585e3465c719aeca77
Hussein-02 <hussein02ab@gmail.com>
2025-04-28 21:41:43 +0300

    Add register in AuthController -register the AuthService in the controller -add the register function

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateDataRequest;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    protected $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function register(CreateDataRequest $request)
+    {
+        $token = $this->authService->registerUser($request);
+        return $this->successResponse($token, 201);
+    }
 }
