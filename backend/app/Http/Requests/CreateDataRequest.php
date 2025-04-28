24d2170dc7c8e45859cd97ece279a3b8422ad787
Hussein-02 <hussein02ab@gmail.com>
2025-04-28 21:36:12 +0300

    Add rules in CreateDataRequest

--- backend/app/Http/Requests/CreateDataRequest.php
+++ backend/app/Http/Requests/CreateDataRequest.php
@@ -11,7 +11,7 @@ class CreateDataRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class CreateDataRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "first_name" => "string|max:255",
+            "last_name" => "string|max:255",
+            "email" => "required|email",
+            "password" => "required|min:8"
         ];
     }
 }
