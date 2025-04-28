87f009401e356b68e377c7205615b75a3f869b88
Hussein-02 <hussein02ab@gmail.com>
2025-04-28 21:32:30 +0300

    Add registerUser Function in AuthService

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -6,4 +6,16 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
-class AuthService {}
+class AuthService
+{
+    public function registerUser($request)
+    {
+        $user = User::create([
+            'username' => $request->username,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return JWTAuth::fromUser($user);
+    }
+}
