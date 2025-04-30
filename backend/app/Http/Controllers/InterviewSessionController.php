09c94cf7727b604318972484283a5e306dfbccd0
Hussein-02 <hussein02ab@gmail.com>
2025-04-30 19:01:33 +0300

    Register interview session service in the interview session controller

--- backend/app/Http/Controllers/InterviewSessionController.php
+++ backend/app/Http/Controllers/InterviewSessionController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateInterviewRequest;
+use App\Services\InterviewSessionService;
 use Illuminate\Http\Request;
 
 class InterviewSessionController extends Controller
 {
+    protected $Service;
+
+    public function __construct(InterviewSessionService $Service)
+    {
+        $this->Service = $Service;
+    }
+
     public function createSession(CreateInterviewRequest $request) {}
 }
