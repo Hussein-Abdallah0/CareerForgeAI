9504a02abe5317bdaab4e57e38ab4cd9e314cab6
Hussein-02 <hussein02ab@gmail.com>
2025-04-30 18:53:42 +0300

    Initialize createSession function with create interview request

--- backend/app/Http/Controllers/InterviewSessionController.php
+++ backend/app/Http/Controllers/InterviewSessionController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateInterviewRequest;
 use Illuminate\Http\Request;
 
 class InterviewSessionController extends Controller
 {
-    //
+    public function createSession(CreateInterviewRequest $request) {}
 }
