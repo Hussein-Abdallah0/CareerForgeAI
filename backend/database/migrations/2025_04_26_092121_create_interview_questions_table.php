e1d70044b995733be5f33f3effa43687b53297de
Hussein-02 <hussein02ab@gmail.com>
2025-05-01 12:05:53 +0300

    Fix timestamps in interview table

--- backend/database/migrations/2025_04_26_092121_create_interview_questions_table.php
+++ backend/database/migrations/2025_04_26_092121_create_interview_questions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->text('question');
             $table->text('user_answer')->nullable();
             // $table->text('ai_comment')->nullable();
-            $table->timestamps();
+            $table->timestamps(0);
         });
     }
 
