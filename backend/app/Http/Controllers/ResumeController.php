14bcf13a48ebc27d30f4c1886afbc804a3a20e0a
Hussein-02 <hussein02ab@gmail.com>
2025-05-01 22:40:38 +0300

    Finish create resume function in resume controller

--- backend/app/Http/Controllers/ResumeController.php
+++ backend/app/Http/Controllers/ResumeController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateResumeRequest;
 use App\Services\ResumeService;
-use Illuminate\Http\Request;
 
 class ResumeController extends Controller
 {
@@ -12,5 +12,15 @@ class ResumeController extends Controller
     public function __construct(ResumeService $Service)
     {
         $this->Service = $Service;
+    }
+
+    public function createResume(CreateResumeRequest $request)
+    {
+        try {
+            $resume = $this->Service->createResume($request);
+            return $this->successResponse($resume, 201);
+        } catch (\Exception $e) {
+            return $this->errorResponse("Failed to create resume: " . $e->getMessage(), 500);
+        }
     }
 }
