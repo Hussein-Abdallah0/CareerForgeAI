f5a8ce7642f0f2776637ea39e4eeb5643e33c78a
Hussein-02 <hussein02ab@gmail.com>
2025-05-01 22:13:13 +0300

    Change resume factory into the new format

--- backend/database/factories/ResumeFactory.php
+++ backend/database/factories/ResumeFactory.php
@@ -20,10 +20,40 @@ class ResumeFactory extends Factory
         return [
             'user_id' => User::factory(),
             'job_title' => $this->faker->jobTitle(),
-            'experience' => $this->faker->paragraphs(3, true),
-            'skills' => implode(', ', $this->faker->words(5)),
-            'education' => $this->faker->sentence(),
-            'tailored_resume' => $this->faker->paragraphs(5, true),
+            'experience' => [
+                [
+                    'company' => $this->faker->company(),
+                    'position' => $this->faker->jobTitle(),
+                    'duration' => $this->faker->dateTimeBetween('-5 years', 'now')->format('Y-m-d') . ' to present',
+                    'responsibilities' => $this->faker->sentences(4)
+                ],
+                [
+                    'company' => $this->faker->company(),
+                    'position' => $this->faker->jobTitle(),
+                    'duration' => $this->faker->dateTimeBetween('-8 years', '-5 years')->format('Y-m-d') . ' to ' . $this->faker->dateTimeBetween('-5 years', '-3 years')->format('Y-m-d'),
+                    'responsibilities' => $this->faker->sentences(4)
+                ]
+            ],
+            'skills' => $this->faker->randomElements([
+                'PHP',
+                'Laravel',
+                'JavaScript',
+                'Vue.js',
+                'React',
+                'MySQL',
+                'PostgreSQL',
+                'Git',
+                'Docker',
+                'AWS'
+            ], 5),
+            'education' => [
+                [
+                    'degree' => $this->faker->randomElement(['BSc', 'MSc', 'PhD']) . ' in ' . $this->faker->randomElement(['Computer Science', 'Software Engineering', 'Information Technology']),
+                    'institution' => $this->faker->university(),
+                    'year' => $this->faker->year()
+                ]
+            ],
+            'summary' => $this->faker->paragraph(),
             'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
         ];
     }
