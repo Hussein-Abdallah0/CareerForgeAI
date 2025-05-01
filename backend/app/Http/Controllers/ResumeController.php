a960b3e89188b122a359f0913816b446a21931d3
Hussein-02 <hussein02ab@gmail.com>
2025-05-01 22:17:24 +0300

    Register resume service in controller

--- backend/app/Http/Controllers/ResumeController.php
+++ backend/app/Http/Controllers/ResumeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ResumeService;
 use Illuminate\Http\Request;
 
 class ResumeController extends Controller
 {
-    //
+    protected $Service;
+
+    public function __construct(ResumeService $Service)
+    {
+        $this->Service = $Service;
+    }
 }
