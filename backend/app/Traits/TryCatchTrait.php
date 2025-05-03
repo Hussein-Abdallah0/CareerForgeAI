aa140f8c053540ff798907a9ab0f03edecc2c3b0
Hussein-02 <hussein02ab@gmail.com>
2025-05-03 23:59:33 +0300

    Finish TryCatchTrait

--- backend/app/Traits/TryCatchTrait.php
+++ backend/app/Traits/TryCatchTrait.php
@@ -2,7 +2,19 @@
 
 namespace App\Traits;
 
+use Throwable;
+
 trait TryCatchTrait
 {
-    //
+    use ResponseTrait;
+
+    public function tryCatchResponse(callable $callback, string $errorMessage = 'An error occurred', int $errorCode = 500)
+    {
+        try {
+            $result = $callback();
+            return $this->successResponse($result);
+        } catch (Throwable $e) {
+            return $this->errorResponse("$errorMessage: " . $e->getMessage(), $errorCode);
+        }
+    }
 }
