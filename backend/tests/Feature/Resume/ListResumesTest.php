b38b51d0983e4a921aeb120598ea0940b8f7cd1d
Hussein-02 <hussein02ab@gmail.com>
2025-05-03 21:20:08 +0300

    Use RefreshDatabase

--- backend/tests/Feature/Resume/ListResumesTest.php
+++ backend/tests/Feature/Resume/ListResumesTest.php
@@ -4,13 +4,13 @@ namespace Tests\Feature\Resume;
 
 use App\Models\Resume;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ListResumesTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     public function test_user_can_list_their_resumes()
     {
