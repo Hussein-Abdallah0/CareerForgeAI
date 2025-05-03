959549b4dd218b51c9e27930cdc717e3a9a29401
Hussein-02 <hussein02ab@gmail.com>
2025-05-03 21:20:43 +0300

    Use RefreshDatabase

--- backend/tests/Feature/Salary/DeleteSalaryAnalysisTest.php
+++ backend/tests/Feature/Salary/DeleteSalaryAnalysisTest.php
@@ -4,13 +4,13 @@ namespace Tests\Feature\Salary;
 
 use App\Models\SalaryAnalysis;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DeleteSalaryAnalysisTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     public function test_user_can_delete_salary_analysis()
     {
