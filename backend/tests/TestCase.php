a0ab5f790d616248c3dca306459f39cc485e8707
Hussein-02 <hussein02ab@gmail.com>
2025-05-03 19:37:34 +0300

    Add jwtAuth method in TestCase

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Auth;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function jwtAuth(User $user)
+    {
+        $token = Auth::attempt([
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $this->withHeader('Authorization', 'Bearer ' . $token);
+
+        return $this;
+    }
 }
