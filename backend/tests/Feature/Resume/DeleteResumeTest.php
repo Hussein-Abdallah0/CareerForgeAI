243e38b58a7153537643d1842c5ba9e4f2595a4a
Hussein-02 <hussein02ab@gmail.com>
2025-05-03 21:20:02 +0300

    Use RefreshDatabase

--- backend/tests/Feature/Resume/DeleteResumeTest.php
+++ backend/tests/Feature/Resume/DeleteResumeTest.php
@@ -4,13 +4,13 @@ namespace Tests\Feature\Resume;
 
 use App\Models\Resume;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DeleteResumeTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     public function test_user_can_delete_resume()
     {
