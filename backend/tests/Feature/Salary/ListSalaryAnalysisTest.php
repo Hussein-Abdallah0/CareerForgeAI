eef82eabd5d6500385e749dcc68aacf7e551a9c0
Hussein-02 <hussein02ab@gmail.com>
2025-05-03 21:20:51 +0300

    Use RefreshDatabase

--- backend/tests/Feature/Salary/ListSalaryAnalysisTest.php
+++ backend/tests/Feature/Salary/ListSalaryAnalysisTest.php
@@ -4,13 +4,13 @@ namespace Tests\Feature\Salary;
 
 use App\Models\SalaryAnalysis;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ListSalaryAnalysisTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     public function test_user_can_list_their_salary_analyses()
     {
