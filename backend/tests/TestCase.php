6cdd684ea412196d9022432b2d219ad73957b825
Hussein-02 <hussein02ab@gmail.com>
2025-05-03 19:59:52 +0300

    Fix test case

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -8,11 +8,18 @@ use Illuminate\Support\Facades\Auth;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        \Artisan::call('migrate:fresh');
+        \Artisan::call('jwt:secret');
+    }
+
     protected function jwtAuth($user, $guard = 'api')
     {
         $token = auth($guard)->attempt([
             'email' => $user->email,
-            'password' => 'password' // Must match factory password
+            'password' => 'password'
         ]);
 
         $this->withHeader('Authorization', 'Bearer ' . $token);
