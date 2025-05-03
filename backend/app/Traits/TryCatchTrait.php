da63d6340758d4970354a9bd01e82a18fd92c8f4
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 00:06:31 +0300

    Add success code to try catch trait

--- backend/app/Traits/TryCatchTrait.php
+++ backend/app/Traits/TryCatchTrait.php
@@ -8,11 +8,11 @@ trait TryCatchTrait
 {
     use ResponseTrait;
 
-    public function tryCatchResponse(callable $callback, string $errorMessage = 'An error occurred', int $errorCode = 500)
+    public function tryCatchResponse(callable $callback, int $successCode = 200, string $errorMessage = 'An error occurred', int $errorCode = 500)
     {
         try {
             $result = $callback();
-            return $this->successResponse($result);
+            return $this->successResponse($result, $successCode);
         } catch (Throwable $e) {
             return $this->errorResponse("$errorMessage: " . $e->getMessage(), $errorCode);
         }
