98dd4bdb00bce861c9ec716844fca11f228f3bce
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 19:20:31 +0300

    Add user can lst their skills test

--- backend/tests/Feature/Skill/ListUserSkillsTest.php
+++ backend/tests/Feature/Skill/ListUserSkillsTest.php
@@ -2,19 +2,28 @@
 
 namespace Tests\Feature\Skill;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Skill;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class ListUserSkillsTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use DatabaseTransactions;
 
-        $response->assertStatus(200);
+    public function test_user_can_list_their_skills()
+    {
+        $user = User::factory()->create();
+        $skills = Skill::factory()->count(2)->create();
+        $user->skills()->attach($skills, ['proficiency' => 3]);
+
+        $response = $this->jwtAuth($user)
+            ->getJson('/api/v1/skill');
+
+        $response->assertStatus(200)
+            ->assertJsonCount(2)
+            ->assertJsonStructure([
+                '*' => ['id', 'name', 'pivot' => ['proficiency']]
+            ]);
     }
 }
