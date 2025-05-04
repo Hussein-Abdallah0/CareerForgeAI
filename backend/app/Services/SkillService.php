e9f7764240967b3ff889171f141cedc3acfd551f
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 18:09:56 +0300

    Fix addUserSkill

--- backend/app/Services/SkillService.php
+++ backend/app/Services/SkillService.php
@@ -2,21 +2,22 @@
 
 namespace App\Services;
 
+use App\Models\Skill;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 
 class SkillService
 {
 
-    public function addUserSkill($user_id, $skill_id, $proficiency)
+    public function addUserSkill($user_id, $skill_name, $proficiency)
     {
+        $skill = Skill::firstOrCreate(['name' => $skill_name]);
         $user = User::findOrFail($user_id);
 
-        if ($user->skills()->where('skill_id', $skill_id)->exists()) {
+        if ($user->skills()->where('skill_id', $skill->id)->exists()) {
             throw new \Exception('User already has this skill', 400);
         }
 
-        $user->skills()->attach($skill_id, ['proficiency' => $proficiency]);
-        return $user->skills()->where('skill_id', $skill_id)->first();
+        $user->skills()->attach($skill->id, ['proficiency' => $proficiency]);
+        return $user->skills()->where('skill_id', $skill->id)->first();
     }
 }
