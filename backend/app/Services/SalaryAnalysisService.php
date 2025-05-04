18ea0766c0620cda697b407bb007f72ccbd895ec
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 21:42:18 +0300

    Add createAnalysis function in service

--- backend/app/Services/SalaryAnalysisService.php
+++ backend/app/Services/SalaryAnalysisService.php
@@ -2,4 +2,23 @@
 
 namespace App\Services;
 
-class SalaryAnalysisService {}
+use App\Models\SalaryAnalysis;
+use Illuminate\Support\Facades\Auth;
+
+class SalaryAnalysisService
+{
+
+    public function createAnalysis($request)
+    {
+        $user_id = Auth::id();
+
+        return SalaryAnalysis::create([
+            'user_id' => $user_id,
+            'job_title' => $request->job_title,
+            'location' => $request->location,
+            'experience_level' => $request->experience_level,
+            'current_salary' => $request->current_salary,
+            'suggested_range' => $request->suggested_range,
+        ]);
+    }
+}
