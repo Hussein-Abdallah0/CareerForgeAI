169a816e5f6d2da18904451be2deb050edd3865c
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 19:17:09 +0300

    Create test for removing user skill

---
+++ backend/tests/Feature/Skill/RemoveUserSkillTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Skill;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RemoveUserSkillTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
