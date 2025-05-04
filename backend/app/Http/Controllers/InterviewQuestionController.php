ef41270f12f254a486d23be6a00f269fbbfeb2d7
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 13:23:21 +0300

    Fix message format

--- backend/app/Http/Controllers/InterviewQuestionController.php
+++ backend/app/Http/Controllers/InterviewQuestionController.php
@@ -18,12 +18,12 @@ class InterviewQuestionController extends Controller
     public function addQuestion(CreateQuestionRequest $request, $session_id)
     {
         $function = fn() => $this->Service->createQuestion($request, $session_id);
-        return $this->tryCatchResponse($function, 201, 'Failed to add question:');
+        return $this->tryCatchResponse($function, 201, 'Failed to add question');
     }
 
     public function answerQuestion(AnswerQuestionRequest $request, $question_id)
     {
         $function = fn() => $this->Service->answerQuestion($request, $question_id);
-        return $this->tryCatchResponse($function, 200, 'Failed to answer question:');
+        return $this->tryCatchResponse($function, 200, 'Failed to answer question');
     }
 }
