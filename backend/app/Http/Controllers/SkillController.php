1526a0ece0c3969b56752767652fc6fbdc0dbe97
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 18:13:48 +0300

    Add listUserSkills in skill controller

--- backend/app/Http/Controllers/SkillController.php
+++ backend/app/Http/Controllers/SkillController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateSkillRequest;
 use App\Services\SkillService;
+use Illuminate\Support\Facades\Auth;
 
 class SkillController extends Controller
 {
@@ -18,6 +19,13 @@ class SkillController extends Controller
     {
         $function = fn() => $this->Service->listAvailableSkills();
         return $this->tryCatchResponse($function, 200, 'Failed to fetch available skills');
+    }
+
+    public function listUserSkills()
+    {
+        $user_id = Auth::id();
+        $function = fn() => $this->Service->listUserSkills($user_id);
+        return $this->tryCatchResponse($function, 200, 'Failed to fetch user skills');
     }
 
     public function addSkill(CreateSkillRequest $request) {}
