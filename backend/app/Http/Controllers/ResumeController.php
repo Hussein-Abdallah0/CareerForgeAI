bb306aeb5391089a2102a657a0766edfe0b6d54d
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 13:23:28 +0300

    Fix message format

--- backend/app/Http/Controllers/ResumeController.php
+++ backend/app/Http/Controllers/ResumeController.php
@@ -18,25 +18,25 @@ class ResumeController extends Controller
     public function createResume(CreateResumeRequest $request)
     {
         $function = fn() => $this->Service->createResume($request);
-        return $this->tryCatchResponse($function, 201, 'Failed to create resume:');
+        return $this->tryCatchResponse($function, 201, 'Failed to create resume');
     }
 
     public function viewResume($resume_id)
     {
         $function = fn() => $this->Service->viewResume($resume_id);
-        return $this->tryCatchResponse($function, 200, 'Failed to fetch resume:');
+        return $this->tryCatchResponse($function, 200, 'Failed to fetch resume');
     }
 
     public function listResumes()
     {
         $user = Auth::user();
         $function = fn() => $this->Service->listResumes($user->id);
-        return $this->tryCatchResponse($function, 200, 'Failed to fetch resumes:');
+        return $this->tryCatchResponse($function, 200, 'Failed to fetch resumes');
     }
 
     public function deleteResume($resume_id)
     {
         $function = fn() => $this->Service->deleteResume($resume_id);
-        return $this->tryCatchResponse($function, 200, 'Failed to delete resume:');
+        return $this->tryCatchResponse($function, 200, 'Failed to delete resume');
     }
 }
