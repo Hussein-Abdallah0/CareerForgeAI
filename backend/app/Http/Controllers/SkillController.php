0565a054ac994398b45cbc015358d78a0858ddbf
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 17:48:05 +0300

    Create SkillService

--- backend/app/Http/Controllers/SkillController.php
+++ backend/app/Http/Controllers/SkillController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateSkillRequest;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
-    //
+    public function addSkill(CreateSkillRequest $request) {}
 }
---
+++ backend/app/Services/SkillService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+class SkillService
+{
+
+    public function addSkill($request) {}
+}
