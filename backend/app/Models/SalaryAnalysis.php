dad37a8bfaab404e717a7b3e97a3d60492aab97a
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 22:29:07 +0300

    Add table name in model

--- backend/app/Models/SalaryAnalysis.php
+++ backend/app/Models/SalaryAnalysis.php
@@ -10,6 +10,8 @@ class SalaryAnalysis extends Model
     /** @use HasFactory<\Database\Factories\SalaryAnalysisFactory> */
     use HasFactory;
 
+    protected $table = 'salary_analysis';
+
     protected $fillable = [
         'user_id',
         'job_title',
