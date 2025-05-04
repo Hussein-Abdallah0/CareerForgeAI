ac67fc21f946155b6c79971c87154b4ddb0b7d6c
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 18:06:59 +0300

    Add addUserSkill function in skillService

--- backend/app/Services/SkillService.php
+++ backend/app/Services/SkillService.php
@@ -2,8 +2,21 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 class SkillService
 {
 
-    public function addSkill($request) {}
+    public function addUserSkill($user_id, $skill_id, $proficiency)
+    {
+        $user = User::findOrFail($user_id);
+
+        if ($user->skills()->where('skill_id', $skill_id)->exists()) {
+            throw new \Exception('User already has this skill', 400);
+        }
+
+        $user->skills()->attach($skill_id, ['proficiency' => $proficiency]);
+        return $user->skills()->where('skill_id', $skill_id)->first();
+    }
 }
