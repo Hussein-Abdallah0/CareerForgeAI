e1076c161258ec93edfe815941305dd3c864d449
Hussein-02 <hussein02ab@gmail.com>
2025-05-04 21:56:30 +0300

    Add createSalaryAnalysis in controller

--- backend/app/Http/Controllers/SalaryAnalysisController.php
+++ backend/app/Http/Controllers/SalaryAnalysisController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateAnalysisRequest;
 use App\Services\SalaryAnalysisService;
 
 class SalaryAnalysisController extends Controller
@@ -11,5 +12,11 @@ class SalaryAnalysisController extends Controller
     public function __construct(SalaryAnalysisService $Service)
     {
         $this->Service = $Service;
+    }
+
+    public function createSalaryAnalysis(CreateAnalysisRequest $request)
+    {
+        $function = fn() => $this->Service->createAnalysis($request);
+        return $this->tryCatchResponse($function, 201, 'Failed to create analysis');
     }
 }
