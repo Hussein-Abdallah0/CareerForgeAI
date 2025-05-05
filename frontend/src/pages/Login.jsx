dfadc50c0008c5e9862eb46280d9dd197a2081ea
Hussein-02 <hussein02ab@gmail.com>
2025-05-05 23:06:53 +0300

    Change Login.jsx to index.css

--- frontend/src/pages/Login.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Login = () => {
-  return <div className="bg-primary">hello</div>;
-};
-
-export default Login;
---
+++ frontend/src/pages/Login/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+import Button from "../../components/Button";
+
+const Login = () => {
+  return <div></div>;
+};
+
+export default Login;
