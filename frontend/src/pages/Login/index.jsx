812f050d21f088e8241f4858ef1212415cf7e422
Hussein-02 <hussein02ab@gmail.com>
2025-05-06 22:36:50 +0300

    Initialize useAuthForm in index login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,9 +1,48 @@
 import React from "react";
 import "./styles.css";
 import Button from "../../components/Button";
+import Input from "../../components/Input";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="login-body">
+      {/* <img src="logo.svg" alt="logo" className="logo" /> */}
+      <div className="login-section">
+        <h1 className="header">Log In</h1>
+        {/* <form className="login-form" onSubmit={(e) => handleSubmit(e, onLoginSuccess)}>
+          <div className="login-input">
+            <label htmlFor="email">Email</label>
+            <input
+              className="input"
+              type="email"
+              id="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="login-input">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="input"
+              value={form.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <button type="submit" className="primary-btn" disabled={isSubmitting}>
+            {isSubmitting ? "Logging in..." : "Log In"}
+          </button>
+        </form> */}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
