53d7fc002cc876cb0688212415aeaa30f57156f1
Hussein-02 <hussein02ab@gmail.com>
2025-05-06 22:33:07 +0300

    Add registerUser function in authService

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -4,3 +4,8 @@ export const loginUser = async (credentials) => {
   const response = await axiosBaseUrl.post("/login", credentials);
   return response.data;
 };
+
+export const registerUser = async (userData) => {
+  const response = await axiosBaseUrl.post("/register", userData);
+  return response.data;
+};
