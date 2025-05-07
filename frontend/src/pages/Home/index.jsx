1495897a04fe4483af245fda7c75e8a3a752b90f
Hussein-02 <hussein02ab@gmail.com>
2025-05-07 23:33:41 +0300

    Finish 1st section in home page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import "./styles.css";
+import Button from "../../components/Button";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Navbar type="home" />
+      <div className="home1">
+        <h1 className="title">Level Up Your Career With AI-Driven Tools</h1>
+        <p>Practice interviews, tailor your resume, and negotiate salaries all powered by AI</p>
+        <Button text="Get Started" onClick={() => navigate("/login")} />
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
