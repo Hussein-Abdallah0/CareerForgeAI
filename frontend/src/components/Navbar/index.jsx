dfbba8cd22f61b56c10c70ac45771e81d7717876
Hussein-02 <hussein02ab@gmail.com>
2025-05-07 17:28:18 +0300

    Finish navbar component

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,21 +1,31 @@
 import React from "react";
+import { CircleUserRound } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import Button from "../Button";
 import "./styles.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user_id");
+    navigate("/");
+  };
   return (
     <div className="navbar">
       <img src="logo-title.svg" alt="logo" className="logo-title" />
       <ul className="nav-ul">
-        <li className="nav-link">Home</li>
-        <li className="nav-link">Features</li>
-        <li className="nav-link">Benefits</li>
+        <li className="nav-link">Dashboard</li>
+        <li className="nav-link">Interview</li>
+        <li className="nav-link">Resume</li>
+        <li className="nav-link">Salary</li>
       </ul>
-      <div className="nav-btn">
-        <Button version="primary-small" text="Login" onClick={() => navigate("/login")} />
-        <Button version="border-small" text="Sign Up" onClick={() => navigate("/signup")} />
+      <div className="dropdown">
+        <CircleUserRound className="nav-user" />
+        <div className="dropdown-content">
+          <a href="#" id="logout" onClick={handleLogout}>
+            Log Out
+          </a>
+        </div>
       </div>
     </div>
   );
