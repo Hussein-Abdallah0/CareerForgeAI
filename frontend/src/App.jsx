4108fd6c4886140c58fdf2e71b2b4cd2b73731e6
Hussein-02 <hussein02ab@gmail.com>
2025-05-07 12:26:55 +0300

    Add route for signup in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
