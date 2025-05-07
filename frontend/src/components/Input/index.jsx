3faf7c7f5ca9cb8140728fc709a9287786d40812
Hussein-02 <hussein02ab@gmail.com>
2025-05-07 12:41:36 +0300

    Add styling for a smaller input for the signup page

--- frontend/src/components/Input/index.jsx
+++ frontend/src/components/Input/index.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "./styles.css";
 
-const Input = ({ label, name, value, type, onChange, required, placeholder }) => {
+const Input = ({
+  label,
+  name,
+  value,
+  type,
+  onChange,
+  required,
+  placeholder,
+  className = "input",
+}) => {
   return (
     <div className="input-div">
       <label htmlFor={name}>{label}</label>
       <input
         type={type}
         name={name}
-        className="input"
+        className={className}
         value={value}
         placeholder={placeholder}
         onChange={onChange}
