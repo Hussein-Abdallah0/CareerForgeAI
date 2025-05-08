5ee9e1260c21a547045899425b7458395d022872
Hussein-02 <hussein02ab@gmail.com>
2025-05-08 22:53:50 +0300

    Finish job title page

--- frontend/src/pages/Interview/JobTitle/index.jsx
+++ frontend/src/pages/Interview/JobTitle/index.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import "./styles.css";
+import { ArrowBigLeft, ArrowLeft, ChevronRight } from "lucide-react";
+import Navbar from "../../../components/Navbar";
 
 const JobTitle = () => {
-  return <div>JobTitle</div>;
+  const jobs = [
+    "Software Engineer",
+    "Data Analytics",
+    "IT Support",
+    "UX Design",
+    "Project Management",
+    "Cyber Security",
+  ];
+  return (
+    <div>
+      <p className="job-title">What field do you want to practice for?</p>
+      <div className="jobs">
+        {jobs.map((job, index) => {
+          return (
+            <div key={index} className="job">
+              {job}
+              <ChevronRight />
+            </div>
+          );
+        })}
+      </div>
+      <a href="/interview">
+        <ArrowLeft className="arrow" />
+      </a>
+    </div>
+  );
 };
 
 export default JobTitle;
