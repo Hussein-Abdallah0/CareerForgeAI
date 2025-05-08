624e114100122fd58605c5f4a814f992b7053e25
Hussein-02 <hussein02ab@gmail.com>
2025-05-08 17:44:29 +0300

    Add route to interview page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Interview from "./pages/Interview";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/interview" element={<Interview />} />
       </Routes>
     </BrowserRouter>
   );
