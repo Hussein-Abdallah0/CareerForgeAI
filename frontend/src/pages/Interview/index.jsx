50d34e7d70b77d229e81fd864377e1ec021c630e
Hussein-02 <hussein02ab@gmail.com>
2025-05-08 17:50:27 +0300

    Finish first section of interview page

--- frontend/src/pages/Interview/index.jsx
+++ frontend/src/pages/Interview/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Button from "../../components/Button";
+import { useNavigate } from "react-router-dom";
+import "./styles.css";
 
 const Interview = () => {
-  return <div>Interview</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Navbar />
+
+      <div className="interview">
+        <h2>Practice Real-Time AI-Powered Interviews</h2>
+        <p>Get instant feedback, improve your responses, and prepare for your dream role</p>
+        <Button text="Start Interview" onClick={() => navigate("/questions")} />
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Interview;
