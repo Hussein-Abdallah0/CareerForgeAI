59158267fcbe7be58c36aa0fe96bb8842506f356
Hussein-02 <hussein02ab@gmail.com>
2025-05-08 22:38:19 +0300

    Add route to job title page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Interview from "./pages/Interview/Interview";
+import JobTitle from "./pages/Interview/JobTitle";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/interview" element={<Interview />} />
+        <Route path="/interview/start" element={<JobTitle />} />
       </Routes>
     </BrowserRouter>
   );
