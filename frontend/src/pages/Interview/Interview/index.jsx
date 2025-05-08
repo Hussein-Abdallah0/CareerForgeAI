6b888a02f354c0fdf7a4a92e4b94df2c31abbadc
Hussein-02 <hussein02ab@gmail.com>
2025-05-08 21:52:24 +0300

    Move interview page files into aN interview folder

--- frontend/src/pages/Interview/Interview/index.jsx
+++ frontend/src/pages/Interview/Interview/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
-import Button from "../../components/Button";
+import Navbar from "../../../components/Navbar";
+import Footer from "../../../components/Footer";
+import Button from "../../../components/Button";
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
 
