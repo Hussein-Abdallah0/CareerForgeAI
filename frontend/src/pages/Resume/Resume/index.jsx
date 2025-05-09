f541eee93ac77fee606128fe93e7b2c4f0f07149
Hussein-02 <hussein02ab@gmail.com>
2025-05-09 15:27:26 +0300

    Finish resume page

--- frontend/src/pages/Resume/Resume/index.jsx
+++ frontend/src/pages/Resume/Resume/index.jsx
@@ -11,10 +11,28 @@ const Resume = () => {
     <div>
       <Navbar />
 
-      <div className="interview">
+      <div className="resume">
         <h2>Tailor Your Resume with AI</h2>
         <p>Get a personalized, ATS-friendly resume crafted for your desired job</p>
         <Button text="Build Resume" onClick={() => navigate("/resume/start")} />
+      </div>
+
+      <div className="instructions-section">
+        <h4 className="resume-how">How It Works?</h4>
+        <div className="instructions">
+          <div>
+            <img src="resume/image14.png" alt="" />
+            <p>Pick field of work</p>
+          </div>
+          <div>
+            <img src="resume/image16.png" alt="" />
+            <p>Fill Information</p>
+          </div>
+          <div>
+            <img src="resume/image15.png" alt="" />
+            <p>Edit and Download Resume</p>
+          </div>
+        </div>
       </div>
 
       <Footer />
