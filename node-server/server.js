adbafad90a2bb56c6b479a39606a57bc5951f8fc
Hussein-02 <hussein02ab@gmail.com>
2025-05-10 17:57:31 +0300

    Remove unnecessary icons

--- node-server/server.js
+++ node-server/server.js
@@ -5,7 +5,7 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on("connection", (ws) => {
   ws.on("message", async (audioBlob) => {
-    console.log("🎧 Received audio blob from client");
+    console.log("Received audio blob from client");
     const { audio, text } = await processAudio(audioBlob);
     ws.send(JSON.stringify({ audio, text }));
   });
