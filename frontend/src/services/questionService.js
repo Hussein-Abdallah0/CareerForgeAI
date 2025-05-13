89aa939eb88d11c0c836b6025126ccf0880fb86f
Hussein-02 <hussein02ab@gmail.com>
2025-05-13 17:57:10 +0300

    Fix axios utils import in question service

--- frontend/src/services/questionService.js
+++ frontend/src/services/questionService.js
@@ -1,4 +1,4 @@
-import axiosBaseUrl from "../../../utils/axios";
+import axiosBaseUrl from "../utils/axios";
 
 export const saveAnswer = async (questionId, answer) => {
   return axiosBaseUrl.patch(`/question/${questionId}/answer`, {
