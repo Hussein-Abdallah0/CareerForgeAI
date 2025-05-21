7d59be2063f5910996b3cfa664601d786808a5d9
Hussein-02 <hussein02ab@gmail.com>
2025-05-21 04:32:18 +0300

    Fix deploy files

--- server-setup.sh
+++ server-setup.sh
@@ -21,7 +21,7 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Install Docker Compose v2.23.0
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" \
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)" \
    -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -sf /usr/local/bin/docker-compose /usr/bin/dc
